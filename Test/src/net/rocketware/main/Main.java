3b7a68317a5895aee6aeb82e181c6d33798c314c
Anon <Anon@BrysPC>
2015-12-14 19:38:58 -0500

    Did some UI work.

--- Test/src/net/rocketware/main/Main.java
+++ Test/src/net/rocketware/main/Main.java
@@ -1,7 +1,16 @@
 package net.rocketware.main;
+import javax.swing.*;
 
 public class Main {
+	public static int WIDTH = 640;
+	public static int HEIGHT = 480;
 	public static void main(String[] args){
-		
+		System.out.println("Class of Empires started...");
+		JFrame f = new JFrame("Class of Empires");
+		UserInterface ui = new UserInterface();
+		f.add(ui);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(WIDTH, HEIGHT);
+		f.setVisible(true);
 	}
 }
