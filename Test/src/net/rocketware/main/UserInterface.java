200af21ac4fbb644b880695c93ca6fbcfddded2f
Anon <Anon@BrysPC>
2015-12-16 15:45:05 -0500

    Optimization for dynamic units.

--- Test/src/net/rocketware/main/UserInterface.java
+++ Test/src/net/rocketware/main/UserInterface.java
@@ -19,14 +19,34 @@ public class UserInterface extends JPanel implements MouseListener, MouseMotionL
 		g.drawOval(x-10,y-10,20,20);
 		g.setColor(Color.BLUE);
 		UnitDebug.MoveUnit(x, y);
-		g.fillOval(UnitDebug.currentX+5, UnitDebug.currentY+5, 10, 10);
-		g.drawString("debug", UnitDebug.currentX+5, UnitDebug.currentY+5);
-		g.setColor(Color.RED);
-		g.fillRect(UnitDebug.currentX+5, UnitDebug.currentY+20, 20, 4);
-		g.setColor(Color.GREEN);
-		g.fillRect(UnitDebug.currentX+5, UnitDebug.currentY+20, UnitDebug.HP*2, 4);
-		g.setColor(Color.BLUE);
-		g.drawString("HP: "+UnitDebug.HP, UnitDebug.currentX+5, UnitDebug.currentY+35);
+		g.drawRect(1, 1, 50, 30);
+		g.drawRect(50, 1, 50, 30);
+		g.drawString(UnitDebug.UnitNames[1], 20, 15);
+		g.drawString(UnitDebug.UnitNames[0], 70, 15);
+		for(int i = 0; i<UnitDebug.UnitCount; i++){
+			if(UnitDebug.UnitTeams[i] == 1){
+				g.setColor(Color.RED);
+				g.fillOval(UnitDebug.UnitX[i]+5, UnitDebug.UnitY[i]+5, 10, 10);
+				g.drawString(UnitDebug.UnitNames[i], UnitDebug.UnitX[i]+5, UnitDebug.UnitY[i]+5);
+			}
+			if(UnitDebug.UnitTeams[i] == 0){
+				g.setColor(Color.BLUE);
+				g.fillOval(UnitDebug.UnitX[i]+5, UnitDebug.UnitY[i]+5, 10, 10);
+				g.drawString(UnitDebug.UnitNames[i], UnitDebug.UnitX[i]+5, UnitDebug.UnitY[i]+5);
+			}
+			g.setColor(Color.RED);
+			g.fillRect(UnitDebug.UnitX[i]+5, UnitDebug.UnitY[i]+20, 20, 4);
+			g.setColor(Color.GREEN);
+			g.fillRect(UnitDebug.UnitX[i]+5, UnitDebug.UnitY[i]+20, UnitDebug.HP[i]*2, 4);
+			if(UnitDebug.UnitTeams[i] == 1){
+				g.setColor(Color.RED);
+				g.drawString("HP: "+UnitDebug.HP[i], UnitDebug.UnitX[i]+5, UnitDebug.UnitY[i]+35);
+			}
+			if(UnitDebug.UnitTeams[i] == 0){
+				g.setColor(Color.BLUE);
+				g.drawString("HP: "+UnitDebug.HP[i], UnitDebug.UnitX[i]+5, UnitDebug.UnitY[i]+35);
+			}
+		}
 		try {
 			Thread.sleep(5);
 		} catch (InterruptedException e) {
@@ -48,8 +68,14 @@ public class UserInterface extends JPanel implements MouseListener, MouseMotionL
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		x = e.getX();
-		y = e.getY();
+		int x2 = e.getX();
+		int y2 = e.getY();
+		if(y2>0&&y2<30&&x2>0&&x2<50){
+			UnitDebug.SelectedUnit=1;
+		}
+		if(y2>0&&y2<30&&x2>50&&x2<100){
+			UnitDebug.SelectedUnit=0;
+		}
 	}
 
 	@Override
